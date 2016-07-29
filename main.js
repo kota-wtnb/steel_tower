b2d4befceb6eea51fdced25fb0158428dde9ed76
kota watanabe”git config --global user.name kota <watta.2co.3kyoku@gmail.com>
2016-07-30 08:57:01 +0900

    ファイルの追加

---
+++ main.js
@@ -0,0 +1,18 @@
+(function() {
+  "use strict";
+
+  var viewer = new Cesium.Viewer("cesium");
+
+  viewer.dataSources.add(Cesium.KmlDataSource.load("tmu_crosswordrally.kml"));
+
+  viewer.camera.setView({
+  destination: Cesium.Cartesian3.fromDegrees(138, 30, 3000000),
+  orientation: {
+    heading: 0, // 水平方向の回転度（ラジアン）
+    pitch: -1.3, // 垂直方向の回転度（ラジアン） 上を見上げたり下を見下ろしたり
+    roll: 0
+  }
+});
+  // コードをここに記述
+
+}());
