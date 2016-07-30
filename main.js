08324e2690ad47076a6eecc79bbadae68ae5b6a8
kota watanabe”git config --global user.name kota <watta.2co.3kyoku@gmail.com>
2016-07-31 02:15:25 +0900

    変更

--- main.js
+++ main.js
@@ -210,11 +210,11 @@
       scale : 5.0
   }));
 
-  
+
   viewer.dataSources.add(Cesium.KmlDataSource.load("kml/steel_tower.kml"));
 
   viewer.camera.setView({
-  destination: Cesium.Cartesian3.fromDegrees(138, 30, 3000000),
+  destination: Cesium.Cartesian3.fromDegrees(139.402755, 35.555896, 3000),
   orientation: {
     heading: 0, // 水平方向の回転度（ラジアン）
     pitch: -1.3, // 垂直方向の回転度（ラジアン） 上を見上げたり下を見下ろしたり
