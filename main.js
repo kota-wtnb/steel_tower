e2e1bdeaebe6b4a5aa9ba685191bbdc71290cf86
kota watanabe”git config --global user.name kota <watta.2co.3kyoku@gmail.com>
2016-07-30 23:26:23 +0900

    変更

--- main.js
+++ main.js
@@ -6,27 +6,38 @@
     timeline : false,         //デフォルトのタイムラインウィジェットをオフにする
     animation : false         //デフォルトのアニメーションウィジェットをオフにする
   });
-
+//1
   var scene = viewer.scene;
 
   var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
-      Cesium.Cartesian3.fromDegrees( 139.41856384277344, 35.55541242883035, 0.0));
+      Cesium.Cartesian3.fromDegrees( 139.402755, 35.55589666666667, 0.0));
   var model = scene.primitives.add(Cesium.Model.fromGltf({
       url : './steel_tower.gltf',
       modelMatrix : modelMatrix,
       scale : 10.0
   }));
+//2
+  var scene = viewer.scene;
+
+  var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+      Cesium.Cartesian3.fromDegrees( 139.40805, 35.55785, 0.0));
+  var model = scene.primitives.add(Cesium.Model.fromGltf({
+      url : './steel_tower_red.gltf',
+      modelMatrix : modelMatrix,
+      scale : 10.0
+  }));
+//3
   //var viewer = new Cesium.Viewer("cesium");
   var scene = viewer.scene;
 
   var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
       Cesium.Cartesian3.fromDegrees( 139.41856384277344, 36.55541242883035, 0.0));
   var model = scene.primitives.add(Cesium.Model.fromGltf({
-      url : './example4.gltf',
+      url : './steel_tower_red.gltf',
       modelMatrix : modelMatrix,
       scale : 10.0
   }));
-  viewer.dataSources.add(Cesium.KmlDataSource.load("kml/tmu_crosswordrally.kml"));
+  viewer.dataSources.add(Cesium.KmlDataSource.load("kml/steel_tower.kml"));
 
   viewer.camera.setView({
   destination: Cesium.Cartesian3.fromDegrees(138, 30, 3000000),
